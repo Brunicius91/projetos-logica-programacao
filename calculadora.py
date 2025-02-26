796d25beeee39d7d351f7c028df948f99db38164
VINICIUS BRUNO ARAUJO SILVA <vinicius_0410@hotmail.com>
2025-02-26 16:58:31 -0300

    Update calculadora.py
    Add mensagem bem vindo

--- calculadora.py
+++ calculadora.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 from re import A
-
+print("Olá, sejá bem vindo.")
 num1 = int(input("Digite o primeiro número: ")) # Recebe o valor do primeiro número e convertendo em int
 num2 = int(input("Digite o segundo número: ")) # Recebe o valor do segundo número e convertendo em int
 operacao = (input("Escolha a operação desejada:\n A para adição\n S para subtração\n M para multiplicação\n D para divisão\n")) # Usuário escolhe qual tipo de operação deseja efetuar: A para adição / S para subtração / M para multiplicação / D para divisão
